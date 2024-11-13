20e56194ab661c128b37ea8fdb6ed465ca3a9b57
luizhsgassis <luizhsgassis.dev@gmail.com>
2024-11-13 20:12:04 -0300

    primeiro merge

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
 </head>
 <body>
     <section>
-<<<<<<< HEAD
         <div class="login_container">
             <div class="login_logo">
                 <h3>MenteSerena</h3>
@@ -22,16 +21,6 @@
                     <input type="password" name="senha" id="senha" required>
                 </div>
                 <a href="#" type="submit">Entrar</a>
-=======
-        <div class="container">
-            <h3>MenteSerena</h3>
-            <form action="login.php" method="post">
-                <label for="login">Login:</label>
-                <input type="text" name="login" id="login" required>
-                <label for="senha">Senha:</label>
-                <input type="password" name="senha" id="senha" required>
-                <button type="submit">Entrar</button>
->>>>>>> bcb9482c86cdf96e2fdaaee839ae0dddb350c102
             </form>
         </div>
     </section>
